e0750543fb45951f3364a96c6c9209ef03a386c4
manjit <manjit.kaur@stud.hs-hannover.de>
2017-04-24 19:03:36 +0200

    database

--- app/src/main/java/com/healthservices/mha/momole/database/MomoleDAO.java
+++ app/src/main/java/com/healthservices/mha/momole/database/MomoleDAO.java
@@ -20,17 +20,22 @@ public class MomoleDAO {
     public static final String TBL_LM_ID = "id";
     public static final String TBL_LM_TSTMP = "tstmp";
     public static final String TBL_LM_DESCRIPTION = "des";
-    public static final String TBL_LM_DIGESTIVPBL = "digestivpbl";
-    public static final String TBL_LM_HEADACHE = "headache";
-    public static final String TBL_LM_SKINPBL = "skinpbl";
-    public static final String TBL_LM_RESPIDISTRESS = "respidistress";
-    public static final String TBL_LM_FEVER = "fever";
+    public static final String TBL_LM_LACTOSE = " lactose";
+    public static final String TBL_LM_GLUTEN = "gluten";
+    public static final String TBL_LM_FRUCTOSE = "fructose";
+    public static final String TBL_LM_HISTAMIN = "histamin";
+
+
 
     public static final String TBL_B = "beschwerden";
     public static final String TBL_B_ID = "id";
     public static final String TBL_B_TSTMP = "tstmp";
     public static final String TBL_B_DESCRIPTION = "des";
-    public static final String TBL_B_CATEGORY = "category";
+    public static final String TBL_B_DIGESTIVPBL = "digestivpbl";
+    public static final String TBL_B_HEADACHE = "headache";
+    public static final String TBL_B_SKINPBL = "skinpbl";
+    public static final String TBL_B_RESPIDISTRESS = "respidistress";
+    public static final String TBL_B_FEVER = "fever";
 
     public static final String TBL_N = "notizen";
     public static final String TBL_N_ID = "id";
@@ -41,17 +46,20 @@ public class MomoleDAO {
             + TBL_LM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + TBL_LM_TSTMP + " INTEGER NOT NULL, "
             + TBL_LM_DESCRIPTION + " TEXT, "
-            + TBL_LM_DIGESTIVPBL + " TEXT, "
-            + TBL_LM_HEADACHE + " TEXT, "
-            + TBL_LM_SKINPBL + " TEXT, "
-            + TBL_LM_RESPIDISTRESS + " TEXT, "
-            + TBL_LM_FEVER + " TEXT";
+            + TBL_LM_LACTOSE + " TEXT, "
+            + TBL_LM_GLUTEN + " TEXT, "
+            + TBL_LM_FRUCTOSE + " TEXT, "
+            + TBL_LM_HISTAMIN + " TEXT, ";
 
     public static final String CREATE_TBL_B = "CREATE TBL " + TBL_B + "("
             + TBL_B_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + TBL_B_TSTMP + " INTEGER NOT NULL, "
             + TBL_B_DESCRIPTION + " TEXT, "
-            + TBL_B_CATEGORY + " TEXT";
+            + TBL_B_DIGESTIVPBL + " TEXT, "
+            + TBL_B_HEADACHE + " TEXT, "
+            + TBL_B_SKINPBL + " TEXT, "
+            + TBL_B_RESPIDISTRESS + " TEXT, "
+            + TBL_B_FEVER + " TEXT";
 
     public static final String CREATE_TBL_N = "CREATE TBL " + TBL_N + "("
             + TBL_N_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
