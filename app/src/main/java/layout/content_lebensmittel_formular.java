f02385da06176058e55c1c94d212ca7822628f00
KathaSu <suhr.katharina@gmail.com>
2017-04-24 13:57:59 +0200

    startbildschirm bearbeitet

--- app/src/main/java/layout/content_lebensmittel_formular.java
+++ app/src/main/java/layout/content_lebensmittel_formular.java
@@ -10,12 +10,10 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-<<<<<<< HEAD
-=======
+
 import com.healthservices.mha.momole.database.MomoleDAO;
 import com.healthservices.mha.momole.database.model.Momole;
 
->>>>>>> master
 public class content_lebensmittel_formular extends Fragment {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
