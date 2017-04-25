15601d6cb4b39f52bc82c7da3f743febeb6190cc
manjit <manjit.kaur@stud.hs-hannover.de>
2017-04-25 22:34:44 +0200

    BeschwerdenDAO

--- app/src/main/java/com/healthservices/mha/momole/database/BeschwerdenDAO.java
+++ app/src/main/java/com/healthservices/mha/momole/database/BeschwerdenDAO.java
@@ -6,6 +6,9 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.healthservices.mha.momole.database.model.Beschwerden;
+import com.healthservices.mha.momole.database.model.Lebensmittel;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,15 +19,126 @@ import java.util.List;
 
 public class BeschwerdenDAO {
 
-    public static final String TAG = "BeschwerdenDAO";
+    // columns of the Beschwerden table
+    public static final String TBL_B = "beschwerden";
+    public static final String TBL_B_ID = "id";
+    public static final String TBL_B_TIME = "time";
+    public static final String TBL_B_DESCRIPTION = "des";
+    public static final String TBL_B_DIGESTIVPBL = "dige";
+    public static final String TBL_B_HEADACHE = "head";
+    public static final String TBL_B_SKINPBL = "skin";
+    public static final String TBL_B_RESPIDISTRESS = "resp";
+    public static final String TBL_B_FEVER = "fev";
 
-    // DB fields
-    private SQLiteDatabase mDatabase;
-    private DBHelper mDbHelper;
-    private Context mcontext;
-    private String[] mAllColumns ={DBHelper.TBL_B_ID, DBHelper.TBL_B_TIME,
-            DBHelper.TBL_B_DESCRIPTION, DBHelper.TBL_B_DIGESTIVPBL, DBHelper.TBL_B_HEADACHE,
-            DBHelper.TBL_B_SKINPBL, DBHelper.TBL_B_RESPIDISTRESS, DBHelper.TBL_B_FEVER};
+    //sql statement of the beschwerden table
+    public static final String CREATE_TBL_B = "CREATE TBL " + TBL_B + "("
+            + TBL_B_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + TBL_B_TIME + " INTEGER NOT NULL, "
+            + TBL_B_DESCRIPTION + " TEXT, "
+            + TBL_B_DIGESTIVPBL + " TEXT, "
+            + TBL_B_HEADACHE + " TEXT, "
+            + TBL_B_SKINPBL + " TEXT, "
+            + TBL_B_RESPIDISTRESS + " TEXT, "
+            + TBL_B_FEVER + " TEXT "
+            + ");";
+
+    private static BeschwerdenDAO instance;
+    private DBHelper dbHelper;
+    private SQLiteDatabase database;
+
+    public BeschwerdenDAO getInstance(Context context){
+        if (instance == null){
+            instance = new BeschwerdenDAO(context);
+        }
+        return instance;
+    }
+
+    private BeschwerdenDAO(Context context){
+        dbHelper = DBHelper.getInstance(context);
+    }
+
+    public void open() throws SQLException{
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close(){
+        dbHelper.close();
+    }
+
+    public void onCreate(SQLiteDatabase database){
+        database.execSQL(CREATE_TBL_B);
+    }
+
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion){
+
+    }
+
+    public Beschwerden getBeschwerden(long id) {
+        open();
+        Cursor cursor = database.query(TBL_B, //Table
+                null, //null returns all columns / fields
+                TBL_B_ID + "=?", //Selection (WHERE [field]=?)
+                new String[]{String.valueOf(id)}, //Selection arguments (selection by id)
+                null, //GroupBy (GROUPY BY [field], e. g. in case of sum([field]))
+                null, //Having, Selection on Group By fields (HAVING [field]=1)
+                null, //Limit, limits the selection, e. g. 10 for 10 entries
+                null); //CancelationSignal
+        if (cursor.moveToFirst()) { //if data is available
+            return readFromCursor(cursor); //read the data
+        }
+        cursor.close();
+        close();
+        return null;
+    }
+
+    public List<Beschwerden> getAllBeschwerden() {
+        open();
+
+        Cursor cursor = database.query(TBL_B, //Table
+                new String[] {TBL_B_ID, TBL_B_TIME, TBL_B_DESCRIPTION, TBL_B_DIGESTIVPBL, TBL_B_HEADACHE, TBL_B_SKINPBL, TBL_B_RESPIDISTRESS, TBL_B_FEVER}, //Fields, null would also return all columns / fields
+                null, //Selection (WHERE [field]=?)
+                null, //Selection arguments (replaces ? in Selection)
+                null, //GroupBy (GROUPY BY [field], e. g. in case of sum([field]))
+                null, //Having, Selection on Group By fields (HAVING [field]=1)
+                null, //Limit, limits the selection, e. g. 10 for 10 entries
+                null); //CancelationSignal
+
+        List<Beschwerden> beschwerden = new LinkedList<>();
+        if (cursor.moveToFirst()) { // read in the the result row by row, if data available
+            while (!cursor.isAfterLast()) {
+                beschwerden.add(readFromCursor(cursor));
+                cursor.moveToNext();
+            }
+        }
+
+        public long addBeschwerden(Beschwerden beschwerden) {
+            open();
+            long ret = database.insert(TBL_B, null, prepareValues(beschwerden));
+            if (ret > 0) {
+                beschwerden.setId(ret);
+            }
+            close();
+            return ret;
+        }
+
+        public int updateBeschwerden(Beschwerden beschwerden) {
+            open();
+            int ret = database.update(TBL_B, //Table
+                    prepareValues(beschwerden), //Values
+                    TBL_B_ID + "=?", //Selection (what data to update)
+                    new String[]{String.valueOf(beschwerden.getId())}); // selection by id
+            close();
+            return ret;
+        }
+
+        public int deleteBeschwerden(Beschwerden beschwerden) {
+            open();
+            int ret = database.delete(TBL_B,
+                    TBL_B_ID + "=?", //Selection (what data to delete)
+                    new String[]{String.valueOf(beschwerden.getId())}); // selection by id
+            close();
+            return ret;
+        }
 
 
 }
