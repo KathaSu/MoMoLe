90134d28f09ab1c4d6e968936a454ef385b0a3fe
manjit <manjit.kaur@stud.hs-hannover.de>
2017-04-25 13:34:18 +0200

    DBHelper

--- app/src/main/java/com/healthservices/mha/momole/database/DBHelper.java
+++ app/src/main/java/com/healthservices/mha/momole/database/DBHelper.java
@@ -1,8 +1,38 @@
 package com.healthservices.mha.momole.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by manji on 25.04.2017.
  */
 
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String TAG = "DBHelper"
+
+    public static final String TBL_LM = "lebensmittel";
+    public static final String TBL_LM_ID = "id";
+    public static final String TBL_LM_TIME = "time";
+    public static final String TBL_LM_DESCRIPTION = "des";
+    public static final String TBL_LM_LACTOSE = "lac";
+    public static final String TBL_LM_GLUTEN = "glu";
+    public static final String TBL_LM_FRUCTOSE = "fru";
+    public static final String TBL_LM_HISTAMIN = "his";
+
+    public static final String TBL_B = "beschwerden";
+    public static final String TBL_B_ID = "id";
+    public static final String TBL_B_TIME = "time";
+    public static final String TBL_B_DESCRIPTION = "des";
+    public static final String TBL_B_DIGESTIVPBL = "dige";
+    public static final String TBL_B_HEADACHE = "head";
+    public static final String TBL_B_SKINPBL = "skin";
+    public static final String TBL_B_RESPIDISTRESS = "resp";
+    public static final String TBL_B_FEVER = "fev";
+
+    public static final String TBL_N = "notizen";
+    public static final String TBL_N_ID = "id";
+    public static final String TBL_N_TIME = "time";
+    public static final String TBL_N_DESCRIPTION = "des";
+
 }
